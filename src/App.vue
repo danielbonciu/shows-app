ee69068d3ceff6247f861910f8dad7882516c490
Daniel <dbonciu97@yahoo.com>
2023-07-30 15:55:13 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import BreakingBadCards from './components/BreakingBadCards.vue';
+</script>
+
+<template>
+  <main>
+    <h1>HERO</h1>
+    <BreakingBadCards />
+  </main>
+</template>
---
+++ src/components/BreakingBadCards.vue
@@ -0,0 +1,9 @@
+<script setup>
+    
+</script>
+
+<template>
+    <div>
+        <h1>Breaking Bad Cards</h1>
+    </div>
+</template>
